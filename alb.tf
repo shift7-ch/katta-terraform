d258dc48e4cf448687459ab455b6fed24564d4bf
David Kocher <dkocher@iterate.ch>
2026-03-12 18:12:22 +0100

    Change the ALB health check to use port 9000 with the /health endpoint.

--- alb.tf
+++ alb.tf
@@ -67,7 +67,8 @@ resource "aws_lb_target_group" "keycloak_ecs_target_group" {
   target_type = "ip"
 
   health_check {
-    path                = "/health/ready"
+    path                = "/health"
+    port                = "9000"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
