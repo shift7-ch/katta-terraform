8c0e89b5b600e148d2c5c123b24579f68a788c2c
chenkins <chenkins44@gmail.com>
2026-03-24 23:49:08 +0100

    Update katta server to changes from upstream 1.5.0-beta1 (pr 116).

--- variables.tf
+++ variables.tf
@@ -102,7 +102,7 @@ variable "keycloak_version" {
 variable "hub_version" {
   description = "Docker image tag for ghcr.io/shift7-ch/katta-server."
   type        = string
-  default = "12546cd-amd64"
+  default = "fc29bff-amd64"
 }
 
 variable "ecs_enable_execute_command" {
